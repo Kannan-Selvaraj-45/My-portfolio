da11909cfa051b9cd5e27a4d60c14f4d7ef3430b
Kannan-Selvaraj-45 <kannanselvaraj4533@gmail.com>
2024-07-23 21:48:52 +0530

    new commits

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
       <div className=" text-white max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold ">
-            I'm a Web Developer
+            I'm a Fullstack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             I am a passionate web developer eager to start my career in building
